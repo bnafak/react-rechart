e25b56958512f07f90ea95e737d7a3605392c275
Barun Nafak <bnafak@gmail.com>
2024-02-07 05:28:53 +0600

    axios option added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Chart from "./Components/Chart/Chart";
 import DaisyNavbar from "./Components/DaisyNavbar/DaisyNavbar";
+import Phones from "./Components/Phones/Phones";
 
 
 function App() {
   return (
     <>
       <DaisyNavbar></DaisyNavbar>
-      <h1 className="text-3xl bg-rose-300 text-center">React Rechart</h1>
+      <h1 className="text-3xl bg-rose-300 text-center p-3">React Rechart</h1>
       <Chart></Chart>
+      <Phones></Phones>
     </>
   );
 }
